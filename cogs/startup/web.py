c0a88e637e2f121a3a5114d17cbc55028bd5014e
NastyPigz <61915438+NastyPigz@users.noreply.github.com>
2021-08-10 17:24:17 -0400

    how do I restore older commits again

--- cogs/startup/web.py
+++ cogs/startup/web.py
@@ -1,99 +1,50 @@
-from aiohttp import web
 from discord.ext import commands, tasks
-import discord
-import os
-import aiohttp
-
-app = web.Application()
-routes = web.RouteTableDef()
+import copy
 
 def setup(bot):
-    bot.add_cog(Webserver(bot))
+    bot.add_cog(WebRequest(bot))
 
-class Webserver(commands.Cog):
+class WebRequest(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.web_server.start()
+        self.vote_request.start()
+        self.update_main.start()
+        self.update_sm.start()
         self.maindb = bot.maindb
+        self.oldmain = copy.deepcopy(bot.maindb)
+        self.oldsm = copy.deepcopy(bot.smdata)
         self.hidden=True
+        self.session = bot.aiohttp_session
         self.smdata = bot.smdata
-
-        @routes.get('/')
-        async def welcome(request):
-            return web.Response(text="Running")
         
-        @routes.get('/vote')
-        async def votewebhook(request):
-          #remember to switch and edit in website.
-          try:
-            user = request.headers.get('user')
-            website = request.headers.get('website')
-            double_vote = request.headers.get('double_vote')
-          except Exception as e:
-            print(e)
-            return web.Response(text='{"status": 404}\n')
-          try:
-            user_ = await self.bot.fetch_user(user)
-          except:
-            return web.Response(text='{"status": 404}\n')
-          try:
-            await user_.send(f"Thanks for voting for me on {website}!")
-            if double_vote:
-              self.maindb[str(user)]["inventory"]["coin_bag"]+=2
-            else:
-              self.maindb[str(user)]["inventory"]["coin_bag"]+=1
-          except:
-            pass
-          return web.Response(text='{"status": 200}\n')
+    @tasks.loop(seconds=1.0)
+    async def vote_request(self):
+      response = await self.session.get("/gv")
+      data = await response.json()
+      user = data.get('user')
+      website = data.get('website')
+      double_vote = data.get('double_vote')
+      user_ = self.bot.get_user(int(user))
+      if user_ is None:
+        print(user, "is ID, he cannot be found.")
+        return
+      try:
+        await user_.send(f"Thanks for voting for me on {website}!")
+        if double_vote:
+          self.maindb[str(user)]["inventory"]["coin_bag"]+=2
+        else:
+          self.maindb[str(user)]["inventory"]["coin_bag"]+=1
+      except:
+        pass
         
-        @routes.get('/smd')
-        async def smd(request):
-          try:
-            user = request.headers.get('user')
-            if user is None:
-              raise Exception
-          except:
-            return web.json_response({"status": 404, "data":"One of the arguments were not satisfied."})
-          if self.smdata.get(str(user)) is None:
-            return web.json_response({"status": 404, "data": "None"})
-          return web.json_response({"status": 200, "data": self.smdata.get(str(user))})
-
-        @routes.get('/api')
-        async def api(request):
-          try:
-            user = request.headers.get('user')
-            data = request.headers.get('data')
-            help_ = request.headers.get('help')
-            if help_:
-              return web.Response(text='{"status": 200, "message":"TO INCLUDE USER PLEASE USE ARGUMENT #USER# EXAMPLE: URL/API/V1?user=USER_ID TO INCLUDE DATA PLEASE USE ARGUMENT #DATA# EXAMPLE: URL/API/V1?user=USER_ID&DATA=STRING FOR DATA SUPPORT PLEASE ASK FOR SUPPORT IN DISCORD.GG/CAPITALISM"}\n')
-          except:
-            return web.Response(text='{"status": 404, "message":"One of the arguments were not satisfied."}\n')
-          if user == None:
-            return web.Response(text='{"status": 404, "message":"One of the arguments were not satisfied."}\n')
-          if not str(user) in self.maindb.keys():
-            return web.Response(text='{"status": 404, "message":"The user did not have a profile"}\n')
-          else:
-            try:
-              if data == None:
-                data_ = self.maindb[str(user)]
-                text={"status":200, "message":data_}
-                return web.json_response(text)
-              data_ = self.maindb[str(user)][str(data)]
-              text={"status": 200, "message":data_}
-              return web.json_response(text)
-            except:
-              return web.Response(text='{"status": 404, "message":"The data provided was not valid"}\n')
-
-        self.webserver_port = os.environ.get('PORT', 5000)
-        app.add_routes(routes)
-
-    @tasks.loop()
-    async def web_server(self):
-        runner = web.AppRunner(app)
-        await runner.setup()
-        site = web.TCPSite(runner, host='0.0.0.0', port=self.webserver_port)
-        await site.start()
-
-    @web_server.before_loop
-    async def web_server_before_loop(self):
-        await self.bot.wait_until_ready()
+    @tasks.loop(seconds=1.0)
+    async def update_main(self):
+      if self.oldmain != self.maindb:
+        await self.session.post("/pm", json=self.maindb)
+        self.oldmain = copy.deepcopy(self.maindb)
+    
+    @tasks.loop(seconds=1.0)
+    async def update_sm(self):
+      if self.oldsm != self.smdata:
+        await self.session.post("/ps", json=self.smdata)
+        self.oldsm = copy.deepcopy(self.smdata)
